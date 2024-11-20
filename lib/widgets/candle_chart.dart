2cffb9b10b956d228958758106e8da53f65fb021
sykang519 <sykang@swrd.cc>
2024-11-20 17:31:30 +0900

    test: api 연동 테스트

--- lib/widgets/candle_chart.dart
+++ lib/widgets/candle_chart.dart
@@ -1,26 +1,68 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:candlesticks/candlesticks.dart';
+import 'package:http/http.dart' as http;
 
-class CandleChart extends StatelessWidget {
+class CandleChart extends StatefulWidget {
+  @override
+  _CandleChartState createState() => _CandleChartState();
+}
+
+class _CandleChartState extends State<CandleChart> {
+  List<Candle> _candles = [];
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchCandles();
+  }
+
+  Future<void> _fetchCandles() async {
+    try {
+      final uri = Uri.parse('http://localhost.stock-service/api/v1/stockDetails/historicalFilter?symbol=005930&interval=1d'); // 백엔드 API URL
+      final response = await http.get(uri);
+
+      if (response.statusCode == 200) {
+        final List<dynamic> jsonData = jsonDecode(response.body);
+        print("데이터왔ㅇ여");
+
+        // JSON 데이터를 Candle 객체 리스트로 변환
+        final fetchedCandles = jsonData.map((item) {
+          return Candle(
+            date: DateTime.parse(item['date']),
+            open: double.parse(item['open']),
+            high: double.parse(item['high']),
+            low: double.parse(item['low']),
+            close: double.parse(item['close']),
+            volume: double.parse(item['volume']),
+          );
+        }).toList();
+
+        setState(() {
+          _candles = fetchedCandles;
+          _isLoading = false;
+        });
+      } else {
+        throw Exception('Failed to fetch candles: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error fetching candles: $e');
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    // 예제 데이터
-    final candles = List.generate(
-      50, // 데이터 길이를 50개 이상으로 설정
-          (index) => Candle(
-        date: DateTime(2023, 11, 15).add(Duration(days: index)),
-        open: 100.0 + index * 2,
-        high: 110.0 + index * 2,
-        low: 95.0 + index * 2,
-        close: 105.0 + index * 2,
-        volume: 5000 + index * 100,
-      ),
-    );
-
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: Candlesticks(
-        candles: candles,
+      child: _isLoading
+          ? Center(child: CircularProgressIndicator()) // 로딩 중 표시
+          : Candlesticks(
+        candles: _candles,
       ),
     );
   }
