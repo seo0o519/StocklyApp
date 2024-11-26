c9b6886ef3a0498f744a67373ac31680660109fa
sykang519 <sykang@swrd.cc>
2024-11-26 17:27:03 +0900

    feat: 알림함 페이지 구현

--- lib/screens/alert_message.dart
+++ lib/screens/alert_message.dart
@@ -7,16 +7,32 @@ class AlertMessage extends StatefulWidget {
   _AlertMessageState createState() => _AlertMessageState();
 }
 
-class _AlertMessageState extends State<AlertMessage>{
-
+class _AlertMessageState extends State<AlertMessage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("알림함")),
-        body: SafeArea(
-            child:
-            AlertMessageList()
-      )
+      appBar: AppBar(title: Text("알림함")),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                SizedBox(width: 10),
+                Icon(Icons.notifications, color: Colors.yellow),
+                SizedBox(width: 8),
+                Text(
+                  "알림 목록",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            SizedBox(height: 16), // 상단 Row와 리스트 간의 간격 추가
+            Expanded( // Column의 남은 공간을 ListView에 할당
+              child: AlertMessageList(),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/widgets/alert_message_list.dart
+++ lib/widgets/alert_message_list.dart
@@ -1,27 +1,60 @@
 import 'package:flutter/material.dart';
-import 'package:stockly/widgets/alert_message_list.dart';
 
 class AlertMessageList extends StatefulWidget {
-
   @override
   _AlertMessageListState createState() => _AlertMessageListState();
 }
 
-class _AlertMessageListState extends State<AlertMessageList>{
+class _AlertMessageListState extends State<AlertMessageList> {
   final List<Map<String, dynamic>> messages = [
     {
       "name": "삼성전자",
       "price": 65000,
-      "date" : "2002.05.19"
+      "date": "2002.05.19"
+    },
+    {
+      "name": "삼성전자",
+      "price": 65000,
+      "date": "2002.05.19"
     }
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: SafeArea(
-            child: Text("hello")
-        )
+    return ListView.builder(
+      padding: const EdgeInsets.all(5.0),
+      itemCount: messages.length,
+      itemBuilder: (context, index) {
+        final data = messages[index];
+        return Container(
+          width: double.infinity, // 너비를 부모에 맞춤
+          decoration: BoxDecoration(
+            border: Border(
+              bottom: BorderSide(
+                color: Colors.grey,
+                width: 0.2,
+              ),
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '${data['name']} (이)가 ${data['price']}원에 도달했어요',
+                  style: TextStyle(fontSize: 18),
+                ),
+                SizedBox(height: 8), // 간격 추가
+                Text(
+                  '${data['date']}',
+                  style: TextStyle(fontSize: 12, color: Colors.grey),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
