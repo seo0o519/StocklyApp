b551578302fa2ebbe277a32c911da4fe5477628d
sykang519 <sykang@swrd.cc>
2024-11-28 17:14:24 +0900

    feat: 검색 기능 구현

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,46 +1,109 @@
 import 'package:flutter/material.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
+  @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  final List<Map<String, dynamic>> companies = [
+    {"name": '삼성전자', "symbol": '005930'},
+    {"name": 'LG', "symbol": '003550'},
+    {"name": 'SK하이닉스', "symbol": '000660'},
+    {"name": '삼성바이오로직스', "symbol": '207940'},
+    {"name": '기아', "symbol": '000270'},
+  ];
+
+  List<Map<String, dynamic>> filteredCompanies = [];
+  final TextEditingController searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    filteredCompanies = companies; // 초기 상태는 전체 회사 리스트 표시
+    searchController.addListener(() {
+      filterCompanies();
+    });
+  }
+
+  void filterCompanies() {
+    final query = searchController.text.toLowerCase();
+    if (query.isEmpty) {
+      setState(() {
+        filteredCompanies = companies; // 검색어가 없으면 모든 회사 표시
+      });
+    } else {
+      setState(() {
+        filteredCompanies = companies
+            .where((company) => company['name']
+            .toLowerCase()
+            .contains(query)) // 회사 이름에서 검색어 포함 여부 확인
+            .toList();
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('검색')),
-      body:
-        GestureDetector(
+      body: GestureDetector(
         onTap: () => FocusScope.of(context).unfocus(),
-        child:
-          Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.all(20), // 여기에 패딩 값 설정
-                child:
-                TextField(
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
-                  keyboardType: TextInputType.text,
-                  decoration: InputDecoration(
-                    hintText: "종목 검색",
-                    hintStyle: TextStyle(color: Colors.grey),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0), // 테두리 둥글게 설정
-                      borderSide: BorderSide(color: Color(0xFFF6F7F9)), // 테두리 색상
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0), // 비활성화 상태에서 둥글게
-                      borderSide: BorderSide(color: Color(0xFFF6F7F9)), // 테두리 색상
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0), // 포커스 상태에서 둥글게
-                      borderSide: BorderSide(color: Color(0xFFF6F7F9)), // 포커스 상태에서 테두리 색상과 두께 설정
-                    ),
-                    fillColor: Color(0xFFF6F7F9), // 배경색 설정
-                    filled: true, // 배경색 활성화
-                    prefixIcon: Icon(Icons.search, color: Colors.blue), // 왼쪽에 검색 아이콘 추가
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: TextField(
+                controller: searchController,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  hintText: "종목 검색",
+                  hintStyle: TextStyle(color: Colors.grey),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                    borderSide: BorderSide(color: Color(0xFFF6F7F9)),
                   ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                    borderSide: BorderSide(color: Color(0xFFF6F7F9)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                    borderSide: BorderSide(color: Color(0xFFF6F7F9)),
+                  ),
+                  fillColor: Color(0xFFF6F7F9),
+                  filled: true,
+                  prefixIcon: Icon(Icons.search, color: Colors.blue),
                 ),
               ),
-            ]
-          ),
-        )
+            ),
+            // 종목 리스트
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                itemCount: filteredCompanies.length,
+                itemBuilder: (context, index) {
+                  final data = filteredCompanies[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text('${index + 1}',
+                            style: TextStyle(color: Colors.blue, fontSize: 22)),
+                        SizedBox(width: 15),
+                        Text('${data['name']}',
+                            style: TextStyle(color: Colors.black, fontSize: 20))
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
